00a0cd77e073049a6d03d38eb6d69bb294e753f0
nihaprezz <sergio13prez@gmail.com>
2019-09-17 16:54:47 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,6 +2,8 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES =["Thriller", "Science Fiction", "Romance"]
+
   def initialize(title)
     @title = title
   end
@@ -10,4 +12,9 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre #adds to the end of the array 
+  end 
 end
