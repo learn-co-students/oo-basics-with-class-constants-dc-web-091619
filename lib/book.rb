03be6620481f247660dd595bd7944bc019741bfe
Antonio Abraham <tony.calablava@gmail.com>
2019-09-17 23:24:37 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,18 @@
 class Book
   attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_reader :title, :genre
 
   def initialize(title)
     @title = title
   end
+  GENRES = []
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = GENRES
+    GENRES << genre
+  end
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,21 @@
 class Shoe
+  require 'pry'
+  BRANDS = []
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
-
+  def brand=(brand)
+    binding.pry
+  end
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+    binding.pry
   end
-
+  
 end
